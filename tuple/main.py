bb7f4905e9998f3952d3dbd368569be8e307db60
mdruhulamin7 <ruhulthisis@gmail.com>
2026-07-11 11:51:08 +0600

    Add initial implementation of tuple examples and comments in main.py

---
+++ tuple/main.py
@@ -0,0 +1,38 @@
+# fruits = ('apple',2, 'banana', 'cherry', 'mango', 'grape','banana')
+
+# # print(fruits[0])   
+# # for fruit in fruits:
+# #     print(fruit)
+
+
+# print(fruits.index(2))
+# print(fruits.count('banana'))
+
+# point = (10, 20)
+# x ,_= point
+# print(x) 
+
+
+# from collections import namedtuple
+
+
+# Student = namedtuple("Student",['name','age','cgpa'])
+
+# s = Student(name = 'Ruhul', age =21 , cgpa= 3.25)
+
+# print(s[2])
+# print(s.age)
+
+
+# def get_stats(numbers):
+#     return min(numbers), max(numbers), sum(numbers) / len(numbers)
+
+# data = [10, 20, 30, 40, 50]
+# minimum, maximum, average = get_stats(data)   # Tuple unpacking
+# print(f'Min: {minimum}, Max: {maximum}, Avg: {average}')
+
+# # Swap variables — Tuple ঎র মাধ্যমে
+# a, b = 10, 20
+# a, b = b, a   # আসলে Tuple swap!
+# print(a, b) 
+
