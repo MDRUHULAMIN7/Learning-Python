c9f26e5dcfebf7e842a7782a1d4ad44a493b2fb1
mdruhulamin7 <ruhulthisis@gmail.com>
2026-07-16 14:19:26 +0600

    Comment out Student class implementation and related code; enhance Person class with additional methods and functionality

--- oop/classes/classes.py
+++ oop/classes/classes.py
@@ -14,25 +14,49 @@
 # print(ruhul.a)
 
 
-class Student:
-    college = "RPI"
+# class Student:
+#     college = "RPI"
    
-    def __init__(self,name,roll):
-        self.name = name
-        self.roll = roll 
+#     def __init__(self,name,roll):
+#         self.name = name
+#         self.roll = roll 
 
-s1 = Student('Ruhul',21)
-s2 = Student('Toha',33)
+# s1 = Student('Ruhul',21)
+# s2 = Student('Toha',33)
 
         
-print(s1.name)
-print(s2.name)
-print(s2.college)
+# print(s1.name)
+# print(s2.name)
+# print(s2.college)
 
 
-# s1.college = 'BUBT'
-Student.college = 'Uttara'
+# # s1.college = 'BUBT'
+# Student.college = 'Uttara'
 
-print(s2.college)
-print(s1.college)
+# print(s2.college)
+# print(s1.college)
 
+class Person:                     # Class definition
+    species = 'Human'             # Class Attribute
+
+    def __init__(self, name):     # Constructor / Instance Attribute
+        self.name = name
+
+    def greet(self):              # Instance Method
+        print(f'হ্যালো, আমি {self.name}')
+
+    @classmethod
+    def info(cls):                # Class Method
+        print(f'Species: {cls.species}')
+
+    @staticmethod
+    def utility():                # Static Method
+        print('Utility function')
+
+# Object তৈরি ও ব্যবহার
+p = Person('Ruhul')
+p.greet()            # হ্যালো, আমি Ruhul
+Person.info()        # Species: Human
+Person.utility()     # Utility function
+print(p.name)        # Ruhul
+print(Person.species) # Human
